cd8c280b36e0cf540baa5b8b1fd51babf1c668be
Hoapooh <hoapooh2912@gmail.com>
2024-06-15 16:33:26 +0700

    fix styles of all components and add new footer UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import FAQ from "./pages/FAQ";
+import FAQ from "./pages/FAQ/FAQ";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AuthNav from "./components/AuthNav/AuthNav";
-import Breadcrumb from "./components/Breadcrumb/Breadcrumb";
 function App() {
 	return (
 		<>
 			<Router>
 				<AuthNav />
 				<Header />
-				<Breadcrumb />
 				<Routes>
 					<Route path="/faq" element={<FAQ />} />
 				</Routes>
--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./Breadcrumb.css";
+import { Container } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function Breadcrumb() {
-	return <div>Breadcrumb</div>;
+export default function Breadcrumb({ children }) {
+	return (
+		<>
+			<Container className="breadcrumb__container" maxWidth={"xl"}>
+				<div className="breadcrumb">
+					<Link className="link__nav home" to="/faq">
+						<i class="fa-solid fa-house"></i> Home
+					</Link>{" "}
+					/ <span className="link__nav active">{children}</span>
+				</div>
+			</Container>
+		</>
+	);
 }
