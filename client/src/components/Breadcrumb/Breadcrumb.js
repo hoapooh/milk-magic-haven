4da9cf0dca63a2c3f5ce6baa570373fa1c8cf9f9
Hoapooh <hoapooh2912@gmail.com>
2024-06-30 15:26:18 +0700

    update styles of Breadcrumb and contact

--- client/src/components/Breadcrumb/Breadcrumb.js
+++ client/src/components/Breadcrumb/Breadcrumb.js
@@ -8,7 +8,7 @@ export default function Breadcrumb({ children }) {
 		<>
 			<Container className="breadcrumb__container" maxWidth={"xl"}>
 				<div className="breadcrumb">
-					<Link className="link__nav home" to="/faq">
+					<Link className="link__nav home" to="/">
 						<i class="fa-solid fa-house"></i> Home
 					</Link>{" "}
 					/ <span className="link__nav active">{children}</span>
