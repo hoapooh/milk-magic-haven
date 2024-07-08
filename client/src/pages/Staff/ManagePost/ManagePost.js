b12f85720b3a3ad73f549bcf25ca20ba9d7e8409
trizo1104 <vominhtri221104@gmail.com>
2024-07-08 22:18:18 +0700

    update get post

--- client/src/pages/Staff/ManagePost/ManagePost.js
+++ client/src/pages/Staff/ManagePost/ManagePost.js
@@ -1,23 +1,21 @@
-import { useTheme } from "@emotion/react";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import {
-  Box,
-  IconButton,
-  Paper,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableFooter,
-  TableHead,
-  TablePagination,
-  TableRow,
-} from "@mui/material";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import { Table } from "antd";
-import React, { useEffect, useState } from "react";
-import LastPageIcon from "@mui/icons-material/LastPage";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableHead from '@mui/material/TableHead';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
 
 function TablePaginationActions(props) {
   const theme = useTheme();
@@ -40,42 +38,34 @@ function TablePaginationActions(props) {
   };
 
   return (
-    <Box sx={{ flexShrink: 0, ml: 2.5, display: "flex" }}>
+    <Box sx={{ flexShrink: 0, ml: 2.5, display: 'flex' }}>
       <IconButton
         onClick={handleFirstPageButtonClick}
         disabled={page === 0}
         aria-label="first page"
       >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
       </IconButton>
       <IconButton
         onClick={handleBackButtonClick}
         disabled={page === 0}
         aria-label="previous page"
       >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="last page"
       >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
       </IconButton>
     </Box>
   );
@@ -87,31 +77,31 @@ TablePaginationActions.propTypes = {
   page: PropTypes.number.isRequired,
   rowsPerPage: PropTypes.number.isRequired,
 };
-export default function ManagePost() {
+
+export default function ManageProduct() {
   const [productList, setProductList] = useState([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
+  const fetchData = () => {
+    fetch(`http://localhost:8000/user/get-all-post`, {
+      method: "GET",
+    })
+      .then(res => {
+        if (!res.ok) throw new Error("Failed to fetch data get product");
+        return res.json();
+      })
+      .then(data => setProductList(data.data))
+      .catch(error => console.error("Error fetching data product:", error));
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:8000/user/get-all-post");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const data = await response.json();
-        setProductList(data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchData();
   }, []);
 
-  console.log(productList);
+  console.log(productList)
 
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - productList.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - productList.length) : 0;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -123,98 +113,55 @@ export default function ManagePost() {
   };
 
   return (
-    <Box display="flex" justifyContent="center" mt={20} ml={-30} mr={15}>
-      <TableContainer
-        component={Paper}
-        sx={{ width: "100%", mx: "auto", mt: 0.5 }}
-      >
-        <Table
-          sx={{ minWidth: 1000, fontSize: "1.2rem" }}
-          aria-label="custom pagination table"
-        >
+    <Box display="flex" justifyContent="center" mt={20} ml={-35} mr={9}>
+      <TableContainer component={Paper} sx={{ width: '100%', mx: 'auto', mt: 0.5 }}>
+        <Table sx={{ minWidth: 1000, fontSize: '1.2rem' }} aria-label="custom pagination table">
           <TableHead>
             <TableRow>
-              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
-                Order ID
-              </TableCell>
-              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
-                User ID
-              </TableCell>
-              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
-                Order Date
-              </TableCell>
-              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
-                Status
-              </TableCell>
-              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
-                Total
-              </TableCell>
+              <TableCell align="center" sx={{ fontSize: '1.2rem' }}>Post ID</TableCell>
+              <TableCell align="center" sx={{ fontSize: '1.2rem' }}>User ID</TableCell>
+              <TableCell align="center" sx={{ fontSize: '1.2rem' }}>Content</TableCell>
+              <TableCell align="center" sx={{ fontSize: '1.2rem' }}>Post Date</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {(rowsPerPage > 0
-              ? productList.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
+              ? productList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               : productList
-            ).map((product) => (
-              <TableRow key={product.product_id}>
-                <TableCell
-                  style={{ width: 260 }}
-                  align="center"
-                  sx={{ fontSize: "1.2rem" }}
-                >
-                  {product.order_id}
+            ).map((product, index) => (
+              <TableRow key={product.post_id}>
+                <TableCell style={{ width: 300 }} align="center" sx={{ fontSize: '1.2rem' }}>
+                  {product.post_id}
                 </TableCell>
-                <TableCell
-                  style={{ width: 260 }}
-                  align="center"
-                  sx={{ fontSize: "1.2rem" }}
-                >
+                <TableCell style={{ width: 260 }} align="center" sx={{ fontSize: '1.2rem' }}>
                   {product.user_id}
                 </TableCell>
-                <TableCell
-                  style={{ width: 260 }}
-                  align="center"
-                  sx={{ fontSize: "1.2rem" }}
-                >
-                  {product.order_date}
+                <TableCell style={{ width: 260 }} align="center" sx={{ fontSize: '1.2rem' }}>
+                  {product.content}
                 </TableCell>
-                <TableCell
-                  style={{ width: 260 }}
-                  align="center"
-                  sx={{ fontSize: "1.2rem" }}
-                >
-                  {product.status}
-                </TableCell>
-                <TableCell
-                  style={{ width: 260 }}
-                  align="center"
-                  sx={{ fontSize: "1.2rem" }}
-                >
-                  {product.total_amount}
+                <TableCell style={{ width: 260 }} align="center" sx={{ fontSize: '1.2rem' }}>
+                  {product.post_date}
                 </TableCell>
               </TableRow>
             ))}
             {emptyRows > 0 && (
               <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={7} />
+                <TableCell colSpan={9} />
               </TableRow>
             )}
           </TableBody>
           <TableFooter>
             <TableRow>
               <TablePagination
-                rowsPerPageOptions={[4, 7]}
-                colSpan={7}
+                rowsPerPageOptions={[5]}
+                colSpan={9}
                 count={productList.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 slotProps={{
                   select: {
                     inputProps: {
-                      "aria-label": "rows per page",
+                      'aria-label': 'rows per page',
                     },
                     native: true,
                   },
@@ -230,3 +177,4 @@ export default function ManagePost() {
     </Box>
   );
 }
+
