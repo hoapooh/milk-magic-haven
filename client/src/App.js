44fe2fe78f334cc3c09a1402a3ee28746620b948
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-07-08 21:54:04 +0700

    update post

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import FAQ from "./pages/FAQ/FAQ";
 import Blog from "./pages/Blog/Blog";
 import HomePage from "./pages/Home/HomePage";
 import Products from "./pages/Shop/Products/Products";
-import Contact from "./pages/Contact/Contact";
+// import Contact from "./pages/Contact/Contact";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
@@ -15,39 +15,39 @@ import Statistic from "./pages/admin/statistic/Statistic";
 import Staff from "./pages/Staff/Staff";
 import Cart from "./pages/Cart/Cart";
 const THEME = createTheme({
-	typography: {
-		fontFamily: `"Baloo Paaji 2", sans-serif`,
-	},
+  typography: {
+    fontFamily: `"Baloo Paaji 2", sans-serif`,
+  },
 });
 
 function App() {
-	return (
-		<>
-			<ThemeProvider theme={THEME}>
-				<Routes>
-					{/* ============ USER VIEW ============ */}
-					<Route path="/" element={<HomePage />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/faq" element={<FAQ />} />
-					<Route path="/blog" element={<Blog />} />
-					<Route path="/shop" element={<Products />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/detail/:id" element={<ProductDetail />} />
-					<Route path="/cart" element={<Cart />} />
+  return (
+    <>
+      <ThemeProvider theme={THEME}>
+        <Routes>
+          {/* ============ USER VIEW ============ */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/shop" element={<Products />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/detail/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
 
-					{/* ============ ADMIN VIEW ============ */}
-					<Route path="/admin" element={<AdminPage />} />
-					<Route path="/addAccount" element={<AddAccount />} />
-					<Route path="/update/:id" element={<UpdateAccount />} />
-					<Route path="/admin/statistic" element={<Statistic />} />
+          {/* ============ ADMIN VIEW ============ */}
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/addAccount" element={<AddAccount />} />
+          <Route path="/update/:id" element={<UpdateAccount />} />
+          <Route path="/admin/statistic" element={<Statistic />} />
 
-					{/* ================== STAFF VIEW ============== */}
-					<Route path="/staff/*" element={<Staff />} />
-				</Routes>
-			</ThemeProvider>
-		</>
-	);
+          {/* ================== STAFF VIEW ============== */}
+          <Route path="/staff/*" element={<Staff />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
--- client/src/pages/Staff/ManagePost/ManagePost.js
+++ client/src/pages/Staff/ManagePost/ManagePost.js
@@ -1,7 +1,232 @@
-import React from 'react'
+import { useTheme } from "@emotion/react";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import {
+  Box,
+  IconButton,
+  Paper,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import { Table } from "antd";
+import React, { useEffect, useState } from "react";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import PropTypes from "prop-types";
 
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5, display: "flex" }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 export default function ManagePost() {
-    return (
-        <div>ManagePost</div>
-    )
+  const [productList, setProductList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/user/get-all-post");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setProductList(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(productList);
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - productList.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <Box display="flex" justifyContent="center" mt={20} ml={-30} mr={15}>
+      <TableContainer
+        component={Paper}
+        sx={{ width: "100%", mx: "auto", mt: 0.5 }}
+      >
+        <Table
+          sx={{ minWidth: 1000, fontSize: "1.2rem" }}
+          aria-label="custom pagination table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
+                Order ID
+              </TableCell>
+              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
+                User ID
+              </TableCell>
+              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
+                Order Date
+              </TableCell>
+              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
+                Status
+              </TableCell>
+              <TableCell align="center" sx={{ fontSize: "1.2rem" }}>
+                Total
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? productList.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : productList
+            ).map((product) => (
+              <TableRow key={product.product_id}>
+                <TableCell
+                  style={{ width: 260 }}
+                  align="center"
+                  sx={{ fontSize: "1.2rem" }}
+                >
+                  {product.order_id}
+                </TableCell>
+                <TableCell
+                  style={{ width: 260 }}
+                  align="center"
+                  sx={{ fontSize: "1.2rem" }}
+                >
+                  {product.user_id}
+                </TableCell>
+                <TableCell
+                  style={{ width: 260 }}
+                  align="center"
+                  sx={{ fontSize: "1.2rem" }}
+                >
+                  {product.order_date}
+                </TableCell>
+                <TableCell
+                  style={{ width: 260 }}
+                  align="center"
+                  sx={{ fontSize: "1.2rem" }}
+                >
+                  {product.status}
+                </TableCell>
+                <TableCell
+                  style={{ width: 260 }}
+                  align="center"
+                  sx={{ fontSize: "1.2rem" }}
+                >
+                  {product.total_amount}
+                </TableCell>
+              </TableRow>
+            ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={7} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[4, 7]}
+                colSpan={7}
+                count={productList.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                slotProps={{
+                  select: {
+                    inputProps: {
+                      "aria-label": "rows per page",
+                    },
+                    native: true,
+                  },
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 }
