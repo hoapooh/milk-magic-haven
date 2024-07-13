c00f1e746bca3eb1d1da7f02b00deef9bac7f8d0
Hoapooh <hoapooh2912@gmail.com>
2024-07-13 08:14:25 +0700

    update code

--- client/src/pages/admin/admin_page/AdminPage.js
+++ client/src/pages/admin/admin_page/AdminPage.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./AdminPage.scss";
 import Sidebar from "../sidebar/Sidebar";
-import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+	Grid,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TablePagination,
+} from "@mui/material";
 import { MdEdit } from "react-icons/md";
 import { FaTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -10,95 +20,153 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function AdminPage() {
-  const baseURL = "http://localhost:8000/admin/get-all-user";
-  const [usersAPI, setUsersAPI] = useState([]);
+	const baseURL = "http://localhost:8000/admin/get-all-user";
+	const [usersAPI, setUsersAPI] = useState([]);
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  useEffect(() => {
-    fetch(baseURL)
-      .then((response) => response.json())
-      .then((data) => setUsersAPI(data.data))
-      .catch((error) => console.log(error));
-  }, []);
+	useEffect(() => {
+		fetch(baseURL)
+			.then((response) => response.json())
+			.then((data) => setUsersAPI(data.data))
+			.catch((error) => console.log(error));
+	}, []);
 
-  const fetchAccount = () => {
-    fetch(baseURL)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Fetched users data:", data);
-        setUsersAPI(data.data);
-      })
-      .catch((error) => console.log(error));
-  };
+	const fetchAccount = () => {
+		fetch(baseURL)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Fetched users data:", data);
+				setUsersAPI(data.data);
+			})
+			.catch((error) => console.log(error));
+	};
 
-  const handleDelete = (user_id) => {
-    console.log("Deleting user with id:", user_id);
-    if (window.confirm("Are you sure you want to delete this account?")) {
-      fetch(`http://localhost:8000/admin/delete-user/${user_id}`, { method: "DELETE" })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP Status: ${response.status}`);
-          }
-          toast.success("Xóa người dùng thành công");
-          fetchAccount();
-        })
-        .catch((error) => console.log("Delete failed: ", error));
-    }
-  };
+	const handleDelete = (user_id) => {
+		console.log("Deleting user with id:", user_id);
+		if (window.confirm("Are you sure you want to delete this account?")) {
+			fetch(`http://localhost:8000/admin/delete-user/${user_id}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+					"x-access-token": localStorage.getItem("accessToken"),
+				},
+			})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error(`HTTP Status: ${response.status}`);
+					}
+					toast.success("Xóa người dùng thành công", {
+						autoClose: 1500,
+					});
+					fetchAccount();
+				})
+				.catch((error) => console.log("Delete failed: ", error));
+		}
+	};
 
-  return (
-    <div className="AdminPage">
-      <ToastContainer />
-      <Grid container spacing={2}>
-        <Grid item md={2}>
-          <Sidebar />
-        </Grid>
-        <Grid item md={10}>
-          <div className="admin__content">
-            <h1 className="admin__title">Quản lý tài khoản</h1>
-            <Link to="/addAccount">
-              <Button type="primary" className="addNewAccount">
-                Add new account
-              </Button>
-            </Link>
-            <TableContainer component={Paper} className="table__container">
-              <Table className="admin__table" aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Tên đăng nhập</TableCell>
-                    <TableCell>Mật khẩu</TableCell>
-                    <TableCell>Email</TableCell>
-                    <TableCell>Vai trò</TableCell>
-                    <TableCell>Action</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {usersAPI.map(
-                    (user) =>
-                      user.status && (
-                        <TableRow>
-                          <TableCell>{user.username}</TableCell>
-                          <TableCell>{user.password}</TableCell>
-                          <TableCell>{user.email}</TableCell>
-                          <TableCell>{user.role_id}</TableCell>
-                          <TableCell className="call-to-action">
-                            <Link to={`/update/${user.user_id}`}>
-                              <button className="action-btn edit">
-                                <MdEdit />
-                              </button>
-                            </Link>
-                            <button className="action-btn trash" onClick={() => handleDelete(user.user_id)}>
-                              <FaTrashAlt />
-                            </button>
-                          </TableCell>
-                        </TableRow>
-                      )
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
+
+	const handleChangeRowsPerPage = (event) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
+
+	return (
+		<div className="AdminPage">
+			<ToastContainer />
+			<Grid container spacing={2}>
+				<Grid item md={2}>
+					<Sidebar />
+				</Grid>
+				<Grid item md={10}>
+					<div className="admin__content">
+						<h1 className="admin__title">Quản lý tài khoản</h1>
+						<Link to="/addAccount">
+							<Button type="primary" className="addNewAccount">
+								Add new account
+							</Button>
+						</Link>
+						<TableContainer
+							component={Paper}
+							className="table__container"
+						>
+							<Table
+								className="admin__table"
+								aria-label="simple table"
+							>
+								<TableHead>
+									<TableRow>
+										<TableCell>Tên đăng nhập</TableCell>
+										<TableCell>Mật khẩu</TableCell>
+										<TableCell>Email</TableCell>
+										<TableCell>Vai trò</TableCell>
+										<TableCell>Action</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{usersAPI
+										.slice(
+											page * rowsPerPage,
+											page * rowsPerPage + rowsPerPage
+										)
+										.map(
+											(user) =>
+												user.status && (
+													<TableRow
+														key={user.user_id}
+													>
+														<TableCell>
+															{user.username}
+														</TableCell>
+														<TableCell>
+															{user.password}
+														</TableCell>
+														<TableCell>
+															{user.email}
+														</TableCell>
+														<TableCell>
+															{user.role_id}
+														</TableCell>
+														<TableCell className="call-to-action">
+															<Link
+																to={`/update/${user.user_id}`}
+															>
+																<button className="action-btn edit">
+																	<MdEdit />
+																</button>
+															</Link>
+															<button
+																className="action-btn trash"
+																onClick={() =>
+																	handleDelete(
+																		user.user_id
+																	)
+																}
+															>
+																<FaTrashAlt />
+															</button>
+														</TableCell>
+													</TableRow>
+												)
+										)}
+								</TableBody>
+							</Table>
+							<TablePagination
+								rowsPerPageOptions={[5, 10, 25]}
+								component="div"
+								count={usersAPI.length}
+								rowsPerPage={rowsPerPage}
+								page={page}
+								onPageChange={handleChangePage}
+								onRowsPerPageChange={handleChangeRowsPerPage}
+							/>
+						</TableContainer>
+					</div>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
--- server/src/middleware/authJwt.middlewares.js
+++ server/src/middleware/authJwt.middlewares.js
@@ -6,95 +6,61 @@ const secretKey = process.env.SECRET_KEY;
 const refreshSecretKey = process.env.REFRESH_SECRET_KEY;
 
 async function authenticateToken(req, res, next) {
-  const token = req.headers["x-access-token"];
-  if (!token) {
-    return res.status(401).json({ message: "Yêu cầu đăng nhập" });
-  }
+	const token = req.headers["x-access-token"];
+	if (!token) {
+		return res.status(401).json({ message: "Yêu cầu đăng nhập" });
+	}
 
-  jwt.verify(token, secretKey, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid token" });
-    }
-    req.user = user;
-    next();
-  });
+	jwt.verify(token, secretKey, (err, user) => {
+		if (err) {
+			return res.status(403).json({ message: "Invalid token" });
+		}
+		req.user = user;
+		next();
+	});
 }
 
 async function generateToken(user_id, role) {
-  const accessToken = jwt.sign({ user_id, role }, secretKey, {
-    expiresIn: "1h",
-  });
-  const refreshToken = jwt.sign({ user_id, role }, refreshSecretKey, {
-    expiresIn: "7d",
-  });
+	const accessToken = jwt.sign({ user_id, role }, secretKey, {
+		expiresIn: "1h",
+	});
+	const refreshToken = jwt.sign({ user_id, role }, refreshSecretKey, {
+		expiresIn: "7d",
+	});
 
-  await storeRefreshToken(refreshToken, user_id);
-  return { accessToken, refreshToken };
+	await storeRefreshToken(refreshToken, user_id);
+	return { accessToken, refreshToken };
 }
 
 async function storeRefreshToken(token, user_id) {
-  const expiryDate = new Date();
-  expiryDate.setDate(expiryDate.getDate() + 7);
+	const expiryDate = new Date();
+	expiryDate.setDate(expiryDate.getDate() + 7);
 
-  const pool = await poolPromise;
-  await pool
-    .request()
-    .input("token", sql.VarChar, token)
-    .input("user_id", sql.Int, user_id)
-    .input("expiryDate", sql.DateTime, expiryDate)
-    .query(
-      "INSERT INTO RefreshTokens (token, user_id, expiryDate) VALUES (@token, @user_id, @expiryDate)"
-    );
-}
-
-async function findRefreshToken(token) {
-  const pool = await poolPromise;
-  const result = await pool
-    .request()
-    .input("token", sql.VarChar, token)
-    .query("SELECT * FROM RefreshTokens WHERE token = @token");
-  return result.recordset[0];
-}
-
-async function removeRefreshToken(token) {
-  const pool = await poolPromise;
-  await pool
-    .request()
-    .input("token", sql.VarChar, token)
-    .query("DELETE FROM RefreshTokens WHERE token = @token");
-}
-
-async function authenticateRefreshToken(req, res, next) {
-  const refreshToken = req.headers["x-refresh-token"];
-  if (!refreshToken) return res.status(403).send("Refresh token is required");
-
-  const storedToken = await findRefreshToken(refreshToken);
-  if (!storedToken) return res.status(403).send("Invalid refresh token");
-
-  jwt.verify(refreshToken, refreshSecretKey, (err, user) => {
-    if (err) return res.status(403).send("Invalid refresh token");
-    req.user = user;
-    next();
-  });
+	const pool = await poolPromise;
+	await pool
+		.request()
+		.input("token", sql.VarChar, token)
+		.input("user_id", sql.Int, user_id)
+		.input("expiryDate", sql.DateTime, expiryDate)
+		.query(
+			"INSERT INTO RefreshTokens (token, user_id, expiryDate) VALUES (@token, @user_id, @expiryDate)"
+		);
 }
 
 function authorizeRole(...allowedRoles) {
-  return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return res
-        .status(403)
-        .json({ message: "Bạn không có quyền truy cập!!!" });
-    }
-    next();
-  };
+	return (req, res, next) => {
+		if (!req.user || !allowedRoles.includes(req.user.role)) {
+			return res
+				.status(403)
+				.json({ message: "Bạn không có quyền truy cập!!!" });
+		}
+		next();
+	};
 }
 
 module.exports = {
-  authenticateToken,
-  authenticateRefreshToken,
-  generateToken,
-  findRefreshToken,
-  storeRefreshToken,
-  removeRefreshToken,
-  authorizeRole,
+	authenticateToken,
+	generateToken,
+	storeRefreshToken,
+	authorizeRole,
 };
